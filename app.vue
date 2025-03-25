115a1a3c090a209568cb3ef294190bcb7f8f1664
Andreas Kugel <andreas.kugel@web.de>
2025-03-25 23:24:27 +0100

    migration intermediate

--- app.vue
+++ app.vue
@@ -1,10 +1,12 @@
 <template>
-  <p v-if="isMobile">This is a mobile device</p>
+    <p v-if="isMobile">This is a mobile device</p>
   <p v-else>This is a desktop device</p>
+  <UApp>
   <NuxtRouteAnnouncer />
     <NuxtLayout>
     <NuxtPage/>    
   </NuxtLayout>
+</UApp>
 
 </template>
 <script setup>
